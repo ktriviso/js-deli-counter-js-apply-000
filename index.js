780ecaf13b7b114ee0db1b319613028bb03fcbe2
Krista Triviso <krista.triviso91@gmail.com>
2018-02-06 19:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeli) {
     let i = 0;
     while (i < katzDeli.length) {
       i++;
-  }
+    }
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   }
