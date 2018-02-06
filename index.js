0997cf688f18a23d9a06e016b28d6dbfa4b5a54f
Krista Triviso <krista.triviso91@gmail.com>
2018-02-06 19:34:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, name){
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+  katzDeliLine.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 
 function nowServing(katzDeli) {
