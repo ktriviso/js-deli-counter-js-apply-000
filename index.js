b1b350fa38b36abfcef93707cc23de6774a60e01
Krista Triviso <krista.triviso91@gmail.com>
2018-02-06 19:30:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function takeANumber(katzDeliLine, name){
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(katzDeli) {
+    let i = 0;
+    while (i < katzDeli.length) {
+      i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+ return (`Currently serving ${katzDeli.shift()}.`);
+}
+
+function currentLine(katzDeliLine) {
+  let i = 0;
+  while (i < katzDeliLine.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i]);
+    i++;
+  }
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  } else
+ return(`The line is currently:` + line);
+}
